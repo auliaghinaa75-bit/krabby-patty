77419b3b27f5238b863c2b379e2677a1c2f39d06
TinoNurcahya <tinonurcahya@gmail.com>
2025-09-16 22:44:42 +0700

    add some resipe

--- Resep.java
+++ Resep.java
@@ -4,5 +4,8 @@ public class Resep {
         System.out.println("------------------");
         System.out.println("1. Tomat");
         System.out.println("2. Ketimun");
+        System.out.println("3. Roti");
+        System.out.println("4. Saus Tomat");
+        System.out.println("5. Daging sapi");
     }
 }
